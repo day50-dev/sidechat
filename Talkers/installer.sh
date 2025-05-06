e8d67b06d6af103c194e7f9acab4c89d350643cf
Chris McKenzie <kristopolous@yahoo.com>
2025-05-06 10:30:10 -0700

    updating installer

--- Talkers/installer.sh
+++ Talkers/installer.sh
@@ -29,7 +29,7 @@ for cmd in sq-add sq-picker screen-query; do
   [[ -e $cmd ]] || ln -s $DIR/$cmd .
 done
 
-pipx install --force streamdown
+pipx install --force streamdown llm
 
 if ! echo $PATH | grep "$insdir" > /dev/null; then
     echo "Now add $insdir to your path because I just blindly put things there. See, I told you this was stupid."
@@ -38,14 +38,31 @@ set +x
 {
 cat <<ENDL
 
-## I Think We're Done
+# I Think We're Done
 
-Well that seems to not have crashed. So here's the key strokes
+Well that seems to not have crashed.
+Here's the key strokes:
 
- * tmux key + h -- chat window
- * tmux key + j -- recent code snippets
+ * **tmux key + h** -- chat window
+ * **tmux key + j** -- recent code snippets
 
-And that's about it
+And that's about it!
+
+Also, there's a few other things you probably need
+
+### fzf
+\`\`\`bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+\`\`\`
+
+see here: [https://github.com/junegunn/fzf](https://github.com/junegunn/fzf)
+
+### simonw's llm
+You'll need to set this up
+
+See here: [https://github.com/simonw/llm](https://github.com/simonw/llm)
 
 ENDL
+
 } | sd
